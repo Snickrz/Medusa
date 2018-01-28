6eaa3b99cb6cf073c517077fff4c6e737318a108
Snickrz <35896033+Snickrz@users.noreply.github.com>
2018-01-28 17:28:00 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,31 +7,4 @@ bot.on("ready", function() {
     console.log("Le bot a bien ete connecte");
 });
 
-ot.login("WP0YTuZTMAEWjJy13HYFAs0-VLGRd3VD");
-
-bot.on('message', function(message) {
-    
-    if(message.content === 'ping') {
-     message.reply('Pong ! ' + bot.ping + ' ms !')  
-    }
-    
-});
-
-bot.on('message', async function(message) {
-    if(message.author.equals(bot.user)) return;
-    
-    if(!message.content.startsWith(PREFIX)) return;
-    
-    var args = message.content.substring(PREFIX.length).split (" ");
-    
-    switch (args[0].toLowerCase()) {
-            
-        case "ping":
-           message.reply('Pong ! ' + bot.ping + ' ms !');
-            break;
-            
-            
-        default:
-            message.reply("Commande invalide, faites !help :D");
-    }
-});
+bot.login("WP0YTuZTMAEWjJy13HYFAs0-VLGRd3VD");
