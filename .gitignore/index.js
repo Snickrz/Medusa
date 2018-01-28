7a1ec703cd1676281469b890facf8bd842f62ae4
Snickrz <35896033+Snickrz@users.noreply.github.com>
2018-01-28 17:31:07 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,9 +2,39 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
+const PREFIX = "!";
+
 bot.on("ready", function() {
-    bot.user.setGame("veiller sur ses enfants");
+    bot.user.setGame("TutoBot, !help");
     console.log("Le bot a bien ete connecte");
 });
+
+bot.on('message', function(message) {
+    
+    if(message.content === 'ping') {
+     message.reply('Pong ! ' + bot.ping + ' ms !')  
+    }
+    
+});
+
+bot.on('message', async function(message) {
+    if(message.author.equals(bot.user)) return;
+    
+    if(!message.content.startsWith(PREFIX)) return;
+    
+    var args = message.content.substring(PREFIX.length).split (" ");
+    
+    switch (args[0].toLowerCase()) {
+            
+        case "ping":
+           message.reply('Pong ! ' + bot.ping + ' ms !');
+            break;
+            
+            
+        default:
+            message.reply("Commande invalide, faites !help :D");
+    }
+});
+    
 
 bot.login("NDA2OTE0Mjc0ODUwNzY2ODQ4.DU-J0g.gbgiQ0GsME_FvGGrrpkrqqpoLhE");
