4cd32298b1a5302bb0df012fd5f9567ab1aeeb6f
Snickrz <35896033+Snickrz@users.noreply.github.com>
2018-01-31 14:28:44 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,22 +1,78 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-var bot = new Discord.Client();
+const PREFIX = '!';
+var dispatcher;
 
-const PREFIX = ";";
+function sendError(message, description) {
+    message.channel.send({embed:{
+        color: 15158332,
+        description: ':x: ' = description
+    }});
+}
 
-bot.on("ready", function() {
-    bot.user.setGame("veiller sur ses enfants");
-    console.log("Le bot a bien ete connecte");
+bot.on('ready', function() {
+    console.log("Bot on")
 });
-       
-bot.on('message', function(message) {
-    
-    if(message.content === 'bonjour') {
 
-     message.reply('Bonjour, fils.')
-    }
-    
-});
+bot.on('message', message => {
+    if(message.content[0] === PREFIX) {
+        let splitMessage = message.content.split(" ");
+
+        if(message.content === '!help') { 
+            const embed = new Discord.RichEmbed();
+
+            embed.setAuthor('Fiche d\'aide :', 'https://t3.ftcdn.net/jpg/00/80/33/32/240_F_80333297_qHGRk9vWYoZ1MrNxaHDxuIzTPi4kclBJ.jpg');
+            embed.addField(':small_blue_diamond: !help', 'Cette fiche d\'aide vous donne l\'intégralité des commandes disponibles, et vous donne leur utilisation.');
+            embed.addField(':small_blue_diamond: !règles', 'Cette commande vous envoie les règles par message privé.');
+            embed.addField(':small_blue_diamond: !poll', 'Vous donne aléatoirement une réponse, soit "Oui", soit "Non", suivi du pourcentage de chance que cette réponse soit apparue.');
+            embed.addField(':small_blue_diamond: !soc', 'Cette commmande, vous indique aléatoirement, "Simple" ou "Classé", cela peut vous etre utile si vous voulez jouer et que vous ne savez pas.');
+            embed.addField(':small_blue_diamond: !twix, !mars, !snickerz', 'Ces commmandes vous indiquent ce que Médusa pense de ses ouvriers.');
+            
+
+            message.channel.send({embed});
+        } else if(message.content === '!règles'){
+            message.author.createDM().then(channel => {
+            const embed = new Discord.RichEmbed();
+            embed.setAuthor('Règles du serveur', 'https://t3.ftcdn.net/jpg/00/80/33/32/240_F_80333297_qHGRk9vWYoZ1MrNxaHDxuIzTPi4kclBJ.jpg');
+            embed.addField('__', ':red_circle: Bonjour à tous, ce serveur est un serveur communautaire, merci de vous respecter les uns les autres et de ne pas être haineux ou irrespectueux sous peine d’une sanction. \n:red_circle: On vous connait ? Il suffit d’envoyer un petit message pour recevoir l’affectation que nous jugerons la plus raisonnable.\n:red_circle: Vous êtes nouveau ? N’hésitez pas a demander un accès aux channels résérvés  (GTA, LMS/PUBG, Rainbow Six Siege, et FiveM). N’hésitez, par ailleurs, pas à demander la création de grades et de salons réservés a de nouveaux jeux que vous jugez utiles.\n:heavy_minus_sign: Les grades administrateur et modérateur ne sont pas accessible au grand public, merci de ne pas demander d’acquérir ces grades.\n:red_circle: - Amusez vous bien sur ce serveur et n’hésitez pas à le partager a vos amis !');
+            channel.send({embed});
+
+            }).catch(console.error);
+        } else if(message.content === '!join') {
+            if(message.member.voiceChannel){
+                message.member.voiceChannel.join().then(message.channel.send('Connecté au salon : \'' + message.member.voiceChannel.name +'\''));
+            } else if (!message.member.voiceChannel) {
+                    message.channel.send('Erreur, vous devez être connecté a un salon vocal.')
+                }
+        } else if(message.content === '!leave') {
+            message.member.voiceChannel.leave();
+        } else if(splitMessage[0] === '!play') {
+            if(splitMessage.length === 2) {
+                if(message.member.voiceChannel) {
+                    message.member.voiceChannel.join().then(connexion => {
+                        dispatcher = connexion.playArbitraryInput(splitMessage[1])
+
+                        dispatcher.on('error', e => {
+                            console.log(e);
+                        });
+
+                        dispatcher.on('end', e => {
+                            console.log("Fin du son.");
+                        });
+                    }).catch(console.log);
+                }
+            }
+        } else if(splitMessage[0] === '!pause') {
+            if(dispatcher !== undefined) {
+                dispatcher.pause();
+            }
+        } else if(splitMessage[0] === '!resume') {
+            if(dispatcher !== undefined) {
+                dispatcher.resume();
+            }
+        }
+}});
 
 bot.on('message', async function(message) {
     if(message.author.equals(bot.user)) return;
@@ -32,7 +88,7 @@ bot.on('message', async function(message) {
             break;
   
         case "twix":
-           message.channel.send('Me parles pas de lui stp.');
+           message.channel.send('Me parles pas de lui stp. \nNon j\'rigole je l\'aime :heart:');
             break;
             
         case "mars":
@@ -44,43 +100,18 @@ bot.on('message', async function(message) {
             break;
             
         case "chaine":
-            message.channel.send('https://www.youtube.com/channel/UCanvsq5uut8-KDm0V026oFQ');
+           message.channel.send('https://www.youtube.com/channel/UCanvsq5uut8-KDm0V026oFQ');
             break;
             
-        case "poll":
-            
-            var yes = Math.floor(Math.random() * (100 - 0)) + 0;
-            var no = 100 - yes;
-            
-            if (yes < no) {
-             message.channel.send("```Non à " + no + "%```" );
-}
-            else if (yes > no) {
-             message.channel.send("```Oui à " + no + "%```" );
-}
-            else{
-             message.channel.send("```50-50```");
-}          
-            break;
-            
-        case "soc":
-            
-            var yes = Math.floor(Math.random() * (100 - 0)) + 0;
-            var no = 100 - yes;
-            
-            if (yes < no) {
-             message.channel.send("```Simple à " + no + "%```" );
-}
-            else if (yes > no) {
-             message.channel.send("```Classé à " + yes + "%```");
-}
-            else{
-             message.channel.send("```Chasse aux Terroristes```");
-}          
-            break;
+        
     }
     
 });
-    
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Bienvenue sur mon serveur ! \nTapez !règles pour reçevoir les règles du serveur par message privé, ' + member.displayName + ' !');
+    }).catch(console.error)
+});
 
 bot.login("NDA2OTE0Mjc0ODUwNzY2ODQ4.DU-J0g.gbgiQ0GsME_FvGGrrpkrqqpoLhE");
